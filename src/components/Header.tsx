32a18afc87a262cf28dc7da0b6cd2b10fa7f1bdc
brigita <b.sabutyte@gmail.com>
2023-11-02 13:05:12 +0100

    added navigation links and styles to the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,20 @@
 import './Header.css'
+import { NavLink } from 'react-router-dom'
 
 const Header: React.FC = ()=>{
     return (
         <>
-        Header
+        <nav>
+        <h1>FlavourQuest</h1>
+<section className='links'>
+<NavLink to='/american'>American</NavLink>
+        <NavLink to='/asian'>Asian</NavLink>
+        <NavLink to='/eastern-european'>Eastern-european</NavLink>
+        <NavLink to='/french'>French</NavLink>
+        <NavLink to='italian'>Italian</NavLink>
+        <NavLink to='mediterranean'>Mediterranian</NavLink>
+</section>
+        </nav>
         </>
     )
 }
